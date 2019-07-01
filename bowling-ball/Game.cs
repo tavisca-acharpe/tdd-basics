71d9991e72a188e868870ff6aef91bad49620c01
tavisca-acharpe <51903447+tavisca-acharpe@users.noreply.github.com>
2019-07-01 15:15:44 +0530

    Update Game.cs

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -1,19 +1,72 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BowlingBall
 {
     public class Game
     {
-        public void Roll(int pins)
+        private int rollCount = 0;
+        private int[] rolls = new int[21];
+    
+        public static void Main(string[] args)
         {
-            throw new NotImplementedException();
-        }
 
-        public int GetScore()
+        }
+        
+        public  void Add(int pins)
         {
-            throw new NotImplementedException();
-        }
+            rolls[rollCount++] = pins;
+         }
 
-    }
+        public  int GetScore()
+        {
+           int score = 0;
+           int rollIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+               if (Strike(rollIndex))
+               {
+                 score = score+ 10 + Bonus(rollIndex);
+                 rollIndex = rollIndex+ 1;
+
+                }
+                else if (Spare(rollIndex))
+                {
+                    score = score + 10 + rolls[rollIndex + 2];
+                  rollIndex = rollIndex+ 2;
+                }
+                else
+                {
+                    score = score + SumOfPins(rollIndex);
+                    rollIndex = rollIndex + 2;
+                 }
+                }
+               return score;
+            }
+
+           private bool Strike(int rollIndex)
+           {
+               return rolls[rollIndex] == 10;
+           }
+
+           private int SumOfPins(int rollIndex)
+           {
+               return rolls[rollIndex] + rolls[rollIndex + 1];
+           }
+
+           private int Bonus(int rollIndex)
+           {
+               return rolls[rollIndex + 1] + rolls[rollIndex + 2];
+           }
+
+           private bool Spare(int rollIndex)
+           {
+               return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+           }
+           
+        }
+    
 }
-
